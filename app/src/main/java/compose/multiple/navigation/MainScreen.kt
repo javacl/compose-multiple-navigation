0425bbe4394d643057ee4d946ca41c8dc99937e4
sadeghi <javacl.sadeghi@gmail.com>
2023-02-02 13:19:02 +0330

    creating graphs

--- app/src/main/java/compose/multiple/navigation/MainScreen.kt
+++ app/src/main/java/compose/multiple/navigation/MainScreen.kt
@@ -5,3 +5,16 @@ import androidx.compose.runtime.Composable
 @Composable
 fun MainScreen() {
 }
+
+object Graph {
+
+    const val ROOT = "root_graph"
+
+    const val AUTH = "auth_graph"
+
+    const val HOME = "home_graph"
+
+    const val SHOP = "shop_graph"
+
+    const val PROFILE = "profile_graph"
+}
