72d2767e8493ea24c6717377fbe09a71165c995c
sadeghi <javacl.sadeghi@gmail.com>
2023-02-02 12:53:59 +0330

    added libs

--- app/src/main/java/compose/multiple/navigation/MainActivity.kt
+++ app/src/main/java/compose/multiple/navigation/MainActivity.kt
@@ -6,38 +6,25 @@ import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Surface
-import androidx.compose.material.Text
-import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.tooling.preview.Preview
 import compose.multiple.navigation.ui.theme.ComposeMultipleNavigationTheme
 
 class MainActivity : ComponentActivity() {
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
+
             ComposeMultipleNavigationTheme {
-                // A surface container using the 'background' color from the theme
+
                 Surface(
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colors.background
                 ) {
-                    Greeting("Android")
+                    MainScreen()
                 }
             }
         }
-    }
-}
-
-@Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
-}
-
-@Preview(showBackground = true)
-@Composable
-fun DefaultPreview() {
-    ComposeMultipleNavigationTheme {
-        Greeting("Android")
     }
 }
---
+++ app/src/main/java/compose/multiple/navigation/MainScreen.kt
@@ -0,0 +1,7 @@
+package compose.multiple.navigation
+
+import androidx.compose.runtime.Composable
+
+@Composable
+fun MainScreen() {
+}
